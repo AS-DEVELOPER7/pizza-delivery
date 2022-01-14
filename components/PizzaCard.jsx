1f952c7c79defabc86b3b004415c5d95e664ad28
AS-DEVELOPER <67372463+AS-DEVELOPER7@users.noreply.github.com>
2022-01-14 23:30:08 +0530

    Update PizzaCard.jsx

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -4,15 +4,16 @@ import React from "react";
 import styles from "../styles/PizzaCard.module.scss";
 const PizzaCard = () => {
   return (
-    <Link href="product/45684">
-    <div className={styles.container}>
-      <Image src="/img/pizza.png" height="500" width="500" />
-      <h1 className={styles.title}>FIORI DE ZUCCA </h1>
-      <span className={styles.price}>$19.90</span>
-      <p className={styles.desc}>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo, et.
-      </p>
-    </div>
+    <Link href="product/45684" passHref={true}>
+      <div className={styles.container}>
+        <Image src="/img/pizza.png" height="500" alt="" width="500" />
+        <h1 className={styles.title}>FIORI DE ZUCCA </h1>
+        <span className={styles.price}>$19.90</span>
+        <p className={styles.desc}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo,
+          et.
+        </p>
+      </div>
     </Link>
   );
 };
